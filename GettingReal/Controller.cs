415b39fd142b17e3351565622894d7dd65fd47e0
Jimmy Martin Henriksen <jimm1034@edu.eal.dk>
2018-11-30 15:59:27 +0100

    Fixer stadig Fixet Controller om til en skabelon for Binding class Følger artefakter

--- GettingReal/Controller.cs
+++ GettingReal/Controller.cs
@@ -10,40 +10,17 @@ namespace GettingReal
     {
         public void ShowProductList()
         {
-            Console.WriteLine("-----------------------------");
-            WarehouseRepo myStuff = new WarehouseRepo();
-            myStuff.ProductList();
-            Console.WriteLine("-----------------------------");
-            Console.Write("\nTryk på en vilkårlig tast, for at vende tilbage til menuen. . .");
-            Console.ReadKey();
-            Console.Clear();
+            //Implementer denne
+        }
+
+        public void UpdateQuantity()
+        {
+
         }
 
         public void RegisterNewProduct()
         {
-            do
-            {
-                Console.WriteLine("-----------------------------");
-
-                Console.Write("Indtast varenavn: ");
-                string productName = Console.ReadLine();
-                Console.Write("Hvor manger er på lager?");
-                string stock = Console.ReadLine();
-
-                int quantity;
-                Int32.TryParse(stock, out quantity);
-
-                Console.WriteLine("Sæt en pris på varen");
-                string setPrice = Console.ReadLine();
-
-                double price;
-                double.TryParse(setPrice, out price);
-
-                WarehouseRepo n = new WarehouseRepo();
-                n.SetNewProduct(productName, quantity, price);
-
-                Console.WriteLine("-----------------------------");
-            } while (true);         
+            //Implementer denne        
         }
     }
 }
