0e110de8c510b939ca940bfc5ab01daf9ba67d1f
Jimmy Martin Henriksen <jimm1034@edu.eal.dk>
2018-11-30 16:09:00 +0100

    Kommentarer til Menu

--- GettingReal/Menu.cs
+++ GettingReal/Menu.cs
@@ -10,60 +10,67 @@ namespace GettingReal
 {
     public class Menu
     {
-        private Controller binding = new Controller();
+        private Controller bridge = new Controller();
 
         public void Show()
         {
-            bool running = true;
-            do
+            bool running = true; //Registrere om programmet skal lukkes hvis switchCasen får inputtet "0" sættes denne til false
+
+            do //Holder menuen åben så længe bool er true
             {
-                ShowMenu();
-                string choice = GetUserChoice();
-                switch (choice)
+                ShowMenu(); //Instance af ShowMenu metode
+                string choice = GetUserChoice(); //Instance af GetUserChoice metode
+                switch (choice) //bruger string som henter GetUserChoice Metoden
                 {
                     case "0":
-                        running = false;
+                        running = false; //Sætter bool til false og lukker programmet
                         break;
                     case "1":
-                        ShowProducts();
+                        ShowProducts(); //Instance af metode 
                         break;
                     case "2":
-                        RegisterNewProduct();
+                        RegisterNewProduct();//Instance af metode 
                         break;
-                    default:
+                    default://Kører hvis userchoice ikke stemmer overens med nogen switch case
                         Console.WriteLine("Der er intet menupunkt for dit valg");
                         Console.WriteLine("prøv igen...");
                         Console.ReadLine();
                         break;
                 }
-            } while (running);
+            } while (running); //Do While loop slutter her
         }
 
-        private void ShowMenu()
+        private void ShowMenu() //Menu udskrift
         {
             Console.WriteLine("Svanninge Malerforretning: Lagersystem");
             Console.WriteLine("");
-            Console.WriteLine("1. Vis Varer");//Implementer Vis varer på lager? -Sådan at varer ikke på lager, skjules fra brugeren?
+            Console.WriteLine("1. Vis Varer");
             Console.WriteLine("2. Opret ny vare i systemet");
             Console.WriteLine("");
             Console.WriteLine("0. Afslut Systemet");
         }
 
-        private string GetUserChoice()
+        private string GetUserChoice() //Input metode
         {
             Console.WriteLine();
             Console.Write("Hvad kunne du tænke dig at gøre?: ");
             return Console.ReadLine();
         }
 
-        public void ShowProducts()
+        public void ShowProducts() //Produkt Oversigt
         {
             Console.Clear();
-            binding.ShowProductList();
+            bridge.ShowProductList();
         }
-        public void RegisterNewProduct()
+
+        public void UpdateQuantity() //Opdater antal 
         {
-            binding.RegisterNewProduct();
+            Console.Clear();
+            bridge.UpdateQuantity();
+        }
+        public void RegisterNewProduct() //Opret nyt produkt
+        {
+            bridge.RegisterNewProduct();
         }
     }
 }
